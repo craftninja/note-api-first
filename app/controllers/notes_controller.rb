e7c3c973465b9c6b592655f2198540ddfc95dd58
EmilyPlatzer <emilyplatzer@gmail.com>
2015-01-27 20:42:22 -0700

    User can hit api for CRUDding notes
    User can GET notes, GET note, PUT notes (new), PATCH note (edit) or DELETE note

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,47 @@
+class NotesController < ApplicationController
+  before_action :set_note, only: [:show, :update, :destroy]
+
+  def index
+    @notes = Note.all
+    render json: @notes
+  end
+
+  def show
+    render json: @note
+  end
+
+  def create
+    @note = Note.new(note_params)
+
+    if @note.save
+      render json: @note
+    else
+      render json: @note.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @note.update(note_params)
+      render json: @note
+    else
+      render json: @note.errors, status: :unprocessable_entity
+    end
+  end
+
+
+  def destroy
+    @note.destroy
+    head :no_content
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_note
+      @note = Note.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def note_params
+      params.require(:note).permit(:title, :body)
+    end
+end
